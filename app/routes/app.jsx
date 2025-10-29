43e67f474ad7b219002f0675700de70d90455f68
Jak <jahmedabeer05@gmail.com>
2025-10-29 20:08:23 +0600

    link shop details page in admin ui (sidebar menu)

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,6 +1,6 @@
-import { Outlet, useLoaderData, useRouteError } from "react-router";
-import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
+import { boundary } from "@shopify/shopify-app-react-router/server";
+import { Outlet, useLoaderData, useRouteError } from "react-router";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
@@ -17,7 +17,7 @@ export default function App() {
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
         <s-link href="/app">Home</s-link>
-        <s-link href="/app/additional">Additional page</s-link>
+        <s-link href="/app/shop-details">Shop details</s-link>
       </s-app-nav>
       <Outlet />
     </AppProvider>
