6a1248acb01bbce56c9ece28fab62063205b9554
Jak <jahmedabeer05@gmail.com>
2025-10-29 20:04:00 +0600

    show shop details in app frontend

--- app/routes/app.shop-details.jsx
+++ app/routes/app.shop-details.jsx
@@ -1,24 +1,24 @@
 import { useLoaderData } from "react-router";
 import { authenticate } from "../shopify.server"; // (https://shopify.dev/docs/api/admin-graphql/latest/queries/shop?example=retrieve-information-about-a-shop)
 
-// Fetch Data from Graphql with Loader Function
+// Fetch Data from Graphql with Loader Function (https://reactrouter.com/start/framework/route-module#loader)
 export const loader = async (args) => {
     const { request } = args;
     const { admin } = await authenticate.admin(request);
     const response = await admin.graphql(
         `#graphql
-  query {
-    shop {
-      id
-      name
-      currencyCode
-      contactEmail
-      myshopifyDomain
-      plan{
-        partnerDevelopment
-      }
-    }
-  }`,
+        query {
+            shop {
+                id
+                name
+                currencyCode
+                contactEmail
+                myshopifyDomain
+                plan{
+                    partnerDevelopment
+                }
+            }
+        }`,
     );
     const json = await response.json();
     const shop = json.data.shop;
@@ -30,7 +30,7 @@ export const loader = async (args) => {
 
 export default function shopDetails() {
 
-    // import data from loader
+    // import data from loader - "Closest route" = the route file you're currently in
     const { shop } = useLoaderData(); // (https://reactrouter.com/api/hooks/useLoaderData#useloaderdata)
 
     console.log(shop)
@@ -38,10 +38,15 @@ export default function shopDetails() {
     return (
         <s-page heading="Extra Page">
 
-            <s-section heading="Extra Page Content">
-                <s-paragraph>
-                    This is the extra page content.
-                </s-paragraph>
+            <s-section heading="Shop Details">
+                <s-unordered-list>
+                    <s-list-item>Shop id: <s-stack><s-badge>{shop.id}</s-badge></s-stack></s-list-item>
+                    <s-list-item>Shop id: <s-stack><s-badge>{shop.name}</s-badge></s-stack></s-list-item>
+                    <s-list-item>Contact email: <s-stack><s-badge>{shop.contactEmail}</s-badge></s-stack></s-list-item>
+                    <s-list-item>Domain: <s-stack><s-badge>{shop.myshopifyDomain}</s-badge></s-stack></s-list-item>
+                    <s-list-item>Currency: <s-stack><s-badge>{shop.currencyCode}</s-badge></s-stack></s-list-item>
+                    <s-list-item>Development store: <s-stack><s-badge>{shop.plan.partnerDevelopment ? "Yes" : "No"}</s-badge></s-stack></s-list-item>
+                </s-unordered-list>
             </s-section>
 
         </s-page>
